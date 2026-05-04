7c124000c0d0a56ae7813ffe9616b7e8cee5d650
Michael Mussato <michimussato@gmail.com>
2026-05-04 14:28:37 +0200

    nox (readmes failed)

--- src/OpenStudioLandscapes/Template/config/models.py
+++ src/OpenStudioLandscapes/Template/config/models.py
@@ -2,14 +2,12 @@ import enum
 import pathlib
 from typing import List
 
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 from pydantic import (
     Field,
     PositiveInt,
 )
-
-from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
-
-from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Template import constants, dist
 
@@ -68,6 +66,7 @@ if __name__ == "__main__":
     CONFIG_STR = Config.get_docs()
 else:
     import yaml
+
     CONFIG_STR = yaml.dump(
         Config.model_json_schema(mode="serialization"),
     )
