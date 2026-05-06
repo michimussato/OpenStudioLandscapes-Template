fd446eef052b79647fc5b7499459b5c6b1a548c4
Michael Mussato <michimussato@gmail.com>
2026-05-06 15:14:09 +0200

    nox

--- src/OpenStudioLandscapes/Template/config/models.py
+++ src/OpenStudioLandscapes/Template/config/models.py
@@ -8,7 +8,7 @@ from pydantic import (
     PositiveInt,
 )
 
-from OpenStudioLandscapes.Template import constants, dist, LOGGER
+from OpenStudioLandscapes.Template import LOGGER, constants, dist
 
 
 class Branches(enum.StrEnum):
